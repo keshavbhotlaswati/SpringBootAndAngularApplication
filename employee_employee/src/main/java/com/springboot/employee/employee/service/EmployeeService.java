59805f0e06e281f855482c6066e0fb2f74e04401
swati <swati.aug97@gmail.com>
2022-01-06 16:11:26 +0530

    Update EmployeeService.java

--- employee_employee/src/main/java/com/springboot/employee/employee/service/EmployeeService.java
+++ employee_employee/src/main/java/com/springboot/employee/employee/service/EmployeeService.java
@@ -5,10 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+
 import org.springframework.stereotype.Service;
 
 import com.springboot.employee.employee.entities.Employee;
