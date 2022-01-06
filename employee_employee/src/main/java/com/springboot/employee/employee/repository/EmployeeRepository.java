f415509ee0e44ab6e879bf82a41d455c3aec1b19
swati <swati.aug97@gmail.com>
2022-01-06 16:10:54 +0530

    Update EmployeeRepository.java

--- employee_employee/src/main/java/com/springboot/employee/employee/repository/EmployeeRepository.java
+++ employee_employee/src/main/java/com/springboot/employee/employee/repository/EmployeeRepository.java
@@ -3,9 +3,7 @@ package com.springboot.employee.employee.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
