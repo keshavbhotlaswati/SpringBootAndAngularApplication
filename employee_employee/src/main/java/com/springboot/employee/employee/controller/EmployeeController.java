e7f6a1b0e443d4fe35159619878a30619f9838da
swati <swati.aug97@gmail.com>
2022-01-06 16:10:02 +0530

    Update EmployeeController.java

--- employee_employee/src/main/java/com/springboot/employee/employee/controller/EmployeeController.java
+++ employee_employee/src/main/java/com/springboot/employee/employee/controller/EmployeeController.java
@@ -3,9 +3,7 @@ package com.springboot.employee.employee.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
