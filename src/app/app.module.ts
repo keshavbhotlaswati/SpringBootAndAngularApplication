e1a227ded951eec35d61639f64d05ca289b1f26b
swati keshavabhotla <swati.aug97@outlook.com>
2022-01-31 10:20:57 +0530

    fourth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 import { HttpClientModule } from '@angular/common/http';
 import { EmployeeComponent } from './employee/employee.component';
 import { UserComponent } from './user/user.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { UserComponent } from './user/user.component';
   ],
   imports: [
     BrowserModule,FormsModule,
-    AppRoutingModule, HttpClientModule
+    AppRoutingModule, HttpClientModule,NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../model/employee';
 import { DataService } from '../data.service';
+import { PageserviceService } from '../pageservice.service';
 
 @Component({
   selector: 'app-employee',
@@ -10,8 +11,16 @@ import { DataService } from '../data.service';
 export class EmployeeComponent implements OnInit {
 
 
-  employees: Employee[] = [];
-  constructor(private dataservice :DataService) { }
+  employees: Employee[]=[];
+  count=3;
+  config: any;
+  pager: any = {};
+
+  // paged items
+  pagedItems: any[]=[];
+  constructor(private dataservice :DataService, private pagerService:PageserviceService) {
+    
+   }
 
   ngOnInit(): void {
  
@@ -28,15 +37,37 @@ export class EmployeeComponent implements OnInit {
   {
       this.employees=response;
       console.log("successfully  completed");
+      console.log(this.employees.length);
+      this.setPage(1);
       console.log(this.employees);
   }
   
   deleteEmployee(employee: Employee): void {
      this.dataservice.deleteEmployee(employee)
-       .subscribe( data => {
+       .subscribe( () => {
         this.employees = this.employees.filter(u => u !== employee);
      })
   };
   
 
+show(){
+  if(this.pager.currentPage!=1){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+ 
+  setPage(page: number) {
+    if (page < 1 || page > this.pager.totalPages) {
+        return;
+    }
+
+    // get pager object from service
+    this.pager = this.pagerService.getPager(this.employees.length, page);
+
+    // get current page of items
+    this.pagedItems = this.employees.slice(this.pager.startIndex, this.pager.endIndex + 1);
+}
 }
